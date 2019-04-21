bdb160b3932e8f56938acb8c7dcd9e4d34205c17
Isabell (Issy) Long <me@issyl0.co.uk>
2019-04-21 13:33:56 +0100

    Give the user feedback when running sync-fork, as it can take a while

--- src/cli/commands/sync-fork.cr
+++ src/cli/commands/sync-fork.cr
@@ -12,12 +12,16 @@ class Commands::SyncFork < Admiral::Command
   def sync_fork(repo)
     repo_dir = "#{Commands::Config.retrieve_value("repo_base_path")}/osc/dev-homebrew/#{repo}"
 
+    puts "Checking out master..."
     checkout_master = Process.run("git", ["checkout", "master"], chdir: repo_dir)
 
+    puts "Fetching the upstream master..."
     fetch_upstream = Process.run("git", ["fetch", "upstream"], chdir: repo_dir)
 
+    puts "Merging upstream into origin..."
     merge_upstream_master = Process.run("git", ["merge", "upstream/master"], chdir: repo_dir)
 
+    puts "Pushing to GitHub..."
     push_origin_master = Process.run("git", ["push", "origin", "master"], chdir: repo_dir)
   end
 end
