f01657538a718b76f149e4071b3a648cac9f9676
Isabell (Issy) Long <me@issyl0.co.uk>
2019-05-04 09:59:05 +0100

    Put pushing synced forks behind a flag
    - I'm going to run the sync fork command from a Concourse pipeline as part of a job, and I don't want to always push to GitHub (because creds and the repetition of the job's timings).

--- src/cli/commands/sync-fork.cr
+++ src/cli/commands/sync-fork.cr
@@ -1,16 +1,20 @@
 class Commands::SyncFork < Admiral::Command
   define_argument repo
+  define_flag no_push : Bool,
+    description: "Whether or not to push to GitHub",
+    default: false,
+    required: false
   define_help
 
   def run
     if %w(brew core cask).includes?(arguments.repo)
-      sync_fork(arguments.repo)
+      sync_fork(arguments.repo, flags.no_push)
     else
       puts "Supported repos are 'brew', 'core' or 'cask'."
     end
   end
 
-  def sync_fork(repo)
+  def sync_fork(repo, no_push)
     if base_path = Commands::Config.retrieve_value("repo_base_path")
       repo_dir = "#{base_path}/osc/dev-homebrew/#{repo}"
     else
@@ -30,7 +34,9 @@ class Commands::SyncFork < Admiral::Command
     puts "Merging upstream into origin..."
     merge_upstream_master = Process.run("git", ["merge", "upstream/master"], chdir: repo_dir)
 
-    puts "Pushing to GitHub..."
-    push_origin_master = Process.run("git", ["push", "origin", "master"], chdir: repo_dir)
+    unless no_push
+      puts "Pushing to GitHub..."
+      push_origin_master = Process.run("git", ["push", "origin", "master"], chdir: repo_dir)
+    end
   end
 end
